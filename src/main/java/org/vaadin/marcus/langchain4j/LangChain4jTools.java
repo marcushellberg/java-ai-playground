261c53b834a93ee9c7009209d1905ea7c05ceb14
LangChain4j <info@langchain4j.dev>
2024-09-11 15:03:14 +0200

    - Update to LangChain4j 0.34.0 - Enable OpenAI structured outputs for tools (strict mode) - Slight refactoring

---
+++ src/main/java/org/vaadin/marcus/langchain4j/LangChain4jTools.java
@@ -0,0 +1,41 @@
+package org.vaadin.marcus.langchain4j;
+
+import dev.langchain4j.agent.tool.Tool;
+import org.springframework.stereotype.Component;
+import org.vaadin.marcus.service.BookingDetails;
+import org.vaadin.marcus.service.FlightService;
+
+import java.time.LocalDate;
+
+@Component
+public class LangChain4jTools {
+
+    private final FlightService service;
+
+    public LangChain4jTools(FlightService service) {
+        this.service = service;
+    }
+
+    @Tool("""
+            Retrieves information about an existing booking,
+            such as the flight date, booking status, departure and arrival airports, and booking class.
+            """)
+    public BookingDetails getBookingDetails(String bookingNumber, String firstName, String lastName) {
+        return service.getBookingDetails(bookingNumber, firstName, lastName);
+    }
+
+    @Tool("""
+            Modifies an existing booking.
+            This includes making changes to the flight date, and the departure and arrival airports.
+            """)
+    public void changeBooking(String bookingNumber, String firstName, String lastName,
+                              LocalDate newFlightDate, String newDepartureAirport, String newArrivalAirport) {
+        service.changeBooking(bookingNumber, firstName, lastName, newFlightDate, newDepartureAirport, newArrivalAirport);
+    }
+
+    @Tool
+    public void cancelBooking(String bookingNumber, String firstName, String lastName) {
+        service.cancelBooking(bookingNumber, firstName, lastName);
+    }
+
+}
